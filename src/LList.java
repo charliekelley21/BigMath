d4f7dc6d79cb0ea038c20be36fd3b79c152404e4
charliekelley21 <charlk21@vt.edu>
2020-07-08 20:39:40 -0400

    mods 7/8/20 8:39pm

--- src/LList.java
+++ src/LList.java
@@ -2,30 +2,36 @@ import java.util.NoSuchElementException;
 
 // Linked List implementation for project 1 CS 3114
 public class LList {
-    private Link head;      // Pointer to List header
-    private Link tail;      // Pointer to List tail
-    private Link curr;      // Pointer to current Link
-    private int listSize;   // Size of List
-    
+    private Link head; // Pointer to List header
+    private Link tail; // Pointer to List tail
+    private Link curr; // Pointer to current Link
+    private int listSize; // Size of List
+
     // Constructors
-    LList() { clear(); }
-    
+    LList() {
+        clear();
+    }
+
+
     // remove all elements
     public void clear() {
-        curr = tail = new Link(null);   // create tail
-        head = new Link(tail);          // create header
+        curr = tail = new Link(null); // create tail
+        head = new Link(tail); // create header
         listSize = 0;
     }
-    
+
+
     // Insert "newDigit" at current position
     public boolean insert(int newDigit) {
         curr.setNext(new Link(curr.element(), curr.next()));
         curr.setElement(newDigit);
-        if (tail == curr) tail = curr.next();   //new tail
+        if (tail == curr)
+            tail = curr.next(); // new tail
         listSize++;
         return true;
     }
-    
+
+
     // Append "newDigit" to List
     public boolean append(int newDigit) {
         tail.setNext(new Link(null));
@@ -34,65 +40,97 @@ public class LList {
         listSize++;
         return true;
     }
-    
+
+
     // Remove and return current element
     public int remove() throws NoSuchElementException {
-        if (curr == tail)   // nothing to remove
-            throw new NoSuchElementException("remove() in LList has current of " + curr + " and size of " + listSize + " that is not a valid element");
-        int removeDigit = curr.element();       // remember digit
+        if (curr == tail) // nothing to remove
+            throw new NoSuchElementException("remove() in LList has current of "
+                + curr + " and size of " + listSize
+                + " that is not a valid element");
+        int removeDigit = curr.element(); // remember digit
         curr.setElement(curr.next().element()); // pull forward the next element
-        if (curr.next() == tail) tail = curr;   // removed last, move tail
-        curr.setNext(curr.next().next());       // point around unneeded digit
-        listSize--;                             // decrease List size by one
-        return removeDigit;                     // return value
-        
+        if (curr.next() == tail)
+            tail = curr; // removed last, move tail
+        curr.setNext(curr.next().next()); // point around unneeded digit
+        listSize--; // decrease List size by one
+        return removeDigit; // return value
+
     }
-    
-    public void moveToStart() { curr = head.next(); }
-    public void moveToEnd() { curr = tail; }
-    public void next() { if (curr != tail) curr = curr.next(); }
-    public int length() { return listSize; }
-    
+
+
+    public void moveToStart() {
+        curr = head.next();
+    }
+
+
+    public void moveToEnd() {
+        curr = tail;
+    }
+
+
+    public void next() {
+        if (curr != tail)
+            curr = curr.next();
+    }
+
+
+    public int length() {
+        return listSize;
+    }
+
+
     // Move curr one step to the left; no change if now at front
     public void prev() {
-        if (head.next() == curr) return;
+        if (head.next() == curr)
+            return;
         Link temp = head;
-        while (temp.next() != curr) temp = temp.next();
+        while (temp.next() != curr)
+            temp = temp.next();
         curr = temp;
     }
-    
+
+
     // Return the position of the current element
     public int currPos() {
         Link temp = head.next();
         int i;
-        for(i = 0; curr != temp; i++)
+        for (i = 0; curr != temp; i++)
             temp = temp.next();
         return i;
     }
-    
-    //Move down List to "pos" position
+
+
+    // Move down List to "pos" position
     public boolean mveToPos(int pos) {
-        if ((pos < 0) || (pos > listSize)) return false;
+        if ((pos < 0) || (pos > listSize))
+            return false;
         curr = head.next();
-        for(int i=0; i<pos; i++) curr = curr.next();
+        for (int i = 0; i < pos; i++)
+            curr = curr.next();
         return true;
     }
-    
+
+
     // return true if current position is at end of list
-    public boolean isAtEnd() { return curr == tail; }
-    
+    public boolean isAtEnd() {
+        return curr == tail;
+    }
+
+
     // return true if List is empty
-    public boolean isEmpty() { return listSize == 0; }
-    
+    public boolean isEmpty() {
+        return listSize == 0;
+    }
+
+
     // return current element value
     public int getValue() throws NoSuchElementException {
         if (curr == tail)
-            throw new NoSuchElementException("getValue() in LList has current of " + curr + " and size of " + listSize + " that is not a valid element");
+            throw new NoSuchElementException(
+                "getValue() in LList has current of " + curr + " and size of "
+                    + listSize + " that is not a valid element");
         return curr.element();
     }
-    
-    // addition for two LinkedLists
-    public LList add() {
-        return new LList();
-    }
+
 }
--- src/Link.java
+++ src/Link.java
@@ -1,16 +1,38 @@
 
 public class Link {
-    private int digit;  //value for this node
-    private Link ptr;   // point to the next node in list
-    
+    private int digit; // value for this node
+    private Link ptr; // point to the next node in list
+
     // Constructors
-    Link(int newDigit, Link newPtr) { digit = newDigit; ptr = newPtr; }
-    Link(Link newPtr) { ptr = newPtr; }
-    
+    Link(int newDigit, Link newPtr) {
+        digit = newDigit;
+        ptr = newPtr;
+    }
+
+
+    Link(Link newPtr) {
+        ptr = newPtr;
+    }
+
+
     // Getters and setters for digit and Link pointer stored
-    int element() { return digit; }
-    int setElement(int newDigit) { return digit = newDigit; }
-    Link next() { return ptr; }
-    Link setNext(Link newPtr) { return ptr = newPtr; }
-    
+    int element() {
+        return digit;
+    }
+
+
+    int setElement(int newDigit) {
+        return digit = newDigit;
+    }
+
+
+    Link next() {
+        return ptr;
+    }
+
+
+    Link setNext(Link newPtr) {
+        return ptr = newPtr;
+    }
+
 }
