9938dcaff265b93bf6422a2bb2bd50b83183e775
barakfinnegan <BJFinn98@vt.edu>
2020-07-15 11:32:03 -0400

    Merging to local

--- src/PreciseIntTest.java
+++ src/PreciseIntTest.java
@@ -3,12 +3,8 @@
  * The test case for the PreciseInt Class.
  * 
  * @author Charlie Kelley (charlk21)
-<<<<<<< HEAD
  * @author Barak Finnegan (bjfinn98)
  * @version 2020.07.13
-=======
- * @version 2020.07.14
->>>>>>> branch 'master' of https://github.com/charliekelley21/BigMath.git
  */
 
 import student.TestCase;
